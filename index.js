2811ae98666f2f1a4bac017c126db3e92a771149
Ward Peeters <ward@coding-tech.com>
2019-11-20 00:51:03 +0100

    add id to worker response

--- index.js
+++ index.js
@@ -4,48 +4,48 @@ const tmpDir = os.tmpdir();
 const wrapWorker = require('./wrap-worker');
 
 exports.STRUCTURED_LOG = async (req, res) => {
-  const { inputPaths, outputDir, args } = req.body;
+  const { id, inputPaths, outputDir, args } = req.body;
   const worker = require('@wardpeet/gatsby-plugin-structured-logging-worker/worker');
 
   const result = await wrapWorker(worker.STRUCTURED_LOG)(inputPaths, outputDir, args);
 
-  res.json(result);
+  res.json({ id, ...result });
 }
 
 exports.ERROR_LOG = async (req, res) => {
-  const { inputPaths, outputDir, args } = req.body;
+  const { id, inputPaths, outputDir, args } = req.body;
   const worker = require('@wardpeet/gatsby-plugin-structured-logging-worker/worker');
 
   const result = await wrapWorker(worker.ERROR_LOG)(inputPaths, outputDir, args);
 
-  res.json(result);
+  res.json({ id, ...result });
 }
 
 exports.IMAGE_PROCESSING = async (req, res) => {
-  const { inputPaths, outputDir, args } = req.body;
+  const { id, inputPaths, outputDir, args } = req.body;
   const worker = require('@wardpeet/gatsby-plugin-sharp-worker/worker');
 
   const result = await wrapWorker(worker.IMAGE_PROCESSING)(inputPaths, outputDir, args);
 
-  res.json(result);
+  res.json({ id, ...result });
 }
 
 
 exports.DOWNLOAD_FILE = async (req, res) => {
-  const { inputPaths, outputDir, args } = req.body;
+  const { id, inputPaths, outputDir, args } = req.body;
   const worker = require('@wardpeet/gatsby-plugin-remote-file/worker');
 
   const result = await wrapWorker(worker.DOWNLOAD_FILE)(inputPaths, outputDir, args);
 
-  res.json(result);
+  res.json({ id, ...result });
 }
 
 exports.GENERATE_SVG = async (req, res) => {
-  const { inputPaths, outputDir, args } = req.body;
+  const { id, inputPaths, outputDir, args } = req.body;
   const worker = require('@wardpeet/gatsby-plugin-sqip-worker/worker');
 
   const result = await wrapWorker(worker.GENERATE_SVG)(inputPaths, outputDir, args);
 
-  res.json(result);
+  res.json({ id, ...result });
 }
 
